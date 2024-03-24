034615ffedb583992084abdb377f47c9dd16a61b
MaahinVPanchal <maahinpanchal@gmail.com>
2024-03-24 17:13:18 +0530

    change in more css for better user perspective

--- src/pages/ImagePage.js
+++ src/pages/ImagePage.js
@@ -1,8 +1,26 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useStateValue } from "../StateProvider";
 import useGoogleSearch from "./useGoogleSearch";
 import Response from "./Response1";
 import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Avatar } from "@mui/material";
+import youtube from "../assets/youtube.png";
+import maps from "../images/maps1.png";
+import drive from "../images/drive.png";
+import lens from "../images/lens1.png";
+import contact from "../images/contact.png";
+import instagram from "../images/instagram1.png";
+import photos from "../images/photos.png";
+import telegram from "../images/telegram1.png";
+import whatsapp from "../images/whatsapp.png";
+import classroom from "../images/classroom.png";
+import snap from "../images/snap.png";
+import sheet from "../images/sheet1.png";
+import message from "../images/message.png";
+import linkedin from "../images/linkedln1.png";
+import twitter from "../assets/twitter.png";
+import facebook from "../assets/facebook-icon.png";
 import Search from "./Search";
 // import SearchIcon from "@mui/icons-material/Search";
 // import DescriptionIcon from "@mui/icons-material/Description";
@@ -19,6 +37,11 @@ function ImagePage() {
   //Mock API CALL
   const data = Response;
 
+  const [isOpen, setIsOpen] = useState(false); // State for dropdown visibility
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen); // Toggle dropdown state on click
+  };
   const [selectedOption, setSelectedOption] = useState("");
 
   // Define handleSelection function to update selected option
@@ -38,7 +61,7 @@ function ImagePage() {
           <div className="searchPage_options">
             <div className="searchPage_optionsLeft">
               <div className="searchPage_optionAll">
-                <Link to="/search">&lt; All</Link>
+                <Link to="/search"> All</Link>
               </div>
               <div className="searchPage_option">
                 {/* <ImageIcon /> */}
@@ -109,26 +132,247 @@ function ImagePage() {
             </div>
           </div>
         </div>
-      </div>
-      {true && (
-        <div className="searchPage_results">
-          <p className="searchPage_resultCount">
-            About {data?.searchInformation?.formattedTotalResults} results (
-            {data?.searchInformation?.formattedSearchTime} seconds) for {term}
-          </p>
+        <div style={{ position: "relative" }}>
+          <div
+            className="imagePage_right"
+            style={{
+              position: "absolute",
+              top: 10,
+              left: 600,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AppsIcon onClick={toggleDropdown} />
+            {isOpen && (
+              <div className="dropMenu">
+                <ul>
+                  <li>
+                    <img
+                      src={youtube}
+                      alt="YouTube"
+                      onClick={() =>
+                        (window.location.href = "https://www.youtube.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>YouTube</span>
+                  </li>
 
-          <div className="searchPage_result">
+                  <li>
+                    <img
+                      src={classroom}
+                      alt="Google classroom"
+                      onClick={() =>
+                        (window.location.href =
+                          "https://edu.google.com/intl/en/workspace-for-education/classroom/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Classroom</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={lens}
+                      alt="Google lens"
+                      onClick={() =>
+                        (window.location.href = "https://lens.google/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Lens</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={telegram}
+                      alt="Telegram"
+                      onClick={() =>
+                        (window.location.href = "https://web.telegram.org/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Telegram</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={whatsapp}
+                      alt="Whatsapp"
+                      onClick={() =>
+                        (window.location.href = "https://www.whatsapp.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Whatsapp</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={drive}
+                      alt="Google Drive"
+                      onClick={() =>
+                        (window.location.href =
+                          "https://www.google.com/intl/en_in/drive/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Drive</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={maps}
+                      alt="Google Maps"
+                      onClick={() =>
+                        (window.location.href = "https://www.google.com/maps/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Maps</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={photos}
+                      alt="Google Photos"
+                      onClick={() =>
+                        (window.location.href =
+                          "https://www.google.com/photos/about/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Photos</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={contact}
+                      alt="Contacts"
+                      onClick={() =>
+                        (window.location.href = "https://contacts.google.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Contacts</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={instagram}
+                      alt="Instagram"
+                      onClick={() =>
+                        (window.location.href = "https://www.instagram.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Instagram</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={linkedin}
+                      alt="Linkedin"
+                      onClick={() =>
+                        (window.location.href = "https://www.linkedin.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Linkedin</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={snap}
+                      alt="Snapchat"
+                      onClick={() =>
+                        (window.location.href = "https://www.snapchat.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Snapchat</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={sheet}
+                      alt="Google sheet"
+                      onClick={() =>
+                        (window.location.href =
+                          "https://www.google.com/sheets/about/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Sheets</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={twitter}
+                      alt="Twitter"
+                      onClick={() =>
+                        (window.location.href = "https://twitter.com/?lang=en")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Twitter</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={facebook}
+                      alt="Facebook"
+                      onClick={() =>
+                        (window.location.href = "https://www.facebook.com/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Facebook</span>
+                  </li>
+
+                  <li>
+                    <img
+                      src={message}
+                      alt="Message"
+                      onClick={() =>
+                        (window.location.href =
+                          "https://messages.google.com/web/")
+                      }
+                      style={{ cursor: "pointer" }}
+                    />
+                    <span>Messages</span>
+                  </li>
+                </ul>
+              </div>
+            )}
+            <Avatar style={{marginLeft:"15px"}}/>
+          </div>
+        </div>
+      </div>
+
+      {true && (
+        <div className="imagePage_results">
+          <div className="imagePage_result">
             <style>
               {`.searchPage_image{
                   object-fit:contain;
                   height:250px;
-                  width:250px
+                  width:250px;
+                  align-items: center;
+                  cursor: pointer;
+                  border: 1.3px solid #ddd;
+                  border-radius: 5px;
+                  transition: all 0.3s ease;
+                  margin:7px;
               }
-              .searchPage_result{
+              .imagePage_result{
                 display:flex;
-                flex-wrap: wrap;
-                margin-right:15px;
-                max-width:100%;
+                flex-wrap:wrap;
+              }
+              .imagePage_results{
+                max-width:2000px;
+                width: 1580px;
               }
               `}
             </style>
